923c4e18c308e05d1ebe1102f45f6ad4a28683af
Alex Kiura <kiuraalex@gmail.com>
2021-07-16 21:07:27 +0300

    Setup application routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Pages from './components/Pages';
+import { RoomContextProvider } from './RoomContextProvider';
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RoomContextProvider>
+      <div>
+        <Pages />
+      </div>
+    </RoomContextProvider>
   );
-}
+};
 
 export default App;
